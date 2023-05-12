e9eb5fffe52786e42cfa85f4a4052ecb0008b04f
ryuiseo <rks85227@gmail.com>
2023-05-12 21:38:34 +0900

    connect mongodb

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,38 +1,32 @@
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import { User } from "./db/models/user_model";
+import dotenv from 'dotenv';
 dotenv.config();
-const { MONGODB_URL } = process.env;
-
-mongoose
-  .connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 
-const saveDummyData = async () => {
-  const user = new User({
-    userName: "John Doe",
-    userNickName: "John Doe",
-    email: "johndoe@example.com",
+// Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.json());
+
+// Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.urlencoded({ extended: false }));
+mongoose
+  .connect(
+    'mongodb+srv://bunny:qwert1234567890@bunnytalk.pcfpjrs.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  )
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error(err);
   });
 
-  try {
-    const savedUser = await user.save();
-    console.log("User saved successfully:", savedUser);
-  } catch (err) {
-    console.error("Error saving user:", err);
-  }
-};
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:3000`);
+});
 
-saveDummyData();
-
-app.get("/", (req, res) => res.send("Hello World!~~"));
-
-export default app;
+export { app };
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,6 +1,9 @@
-import app from "./app";
+import 'dotenv/config';
+import { app } from './app';
 
-const { PORT } = process.env;
-const port = PORT || 3000;
+// .env 파일에 예를 들어 PORT="3000" 을 작성하면, process.env.PORT가 3000이 됨
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+});
