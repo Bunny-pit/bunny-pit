c2f47fd56054b880db653d0ac3490f72519dc879
H0onnn <116232939+H0onnn@users.noreply.github.com>
2023-05-15 20:28:53 +0900

    feat : post_router 작성

---
+++ BACKEND/db/routers/post_router.js
@@ -0,0 +1,20 @@
+import { postService } from "../services/post_service.js";
+import { Router } from "express";
+const postRouter = new Router();
+
+// 게시물 작성
+postRouter.post("/new-post", postService.createPost());
+
+// 게시물 수정
+postRouter.patch("/update", postService.updatePost());
+
+// 게시물 삭제
+postRouter.delete("/delete", postService.deletePost());
+
+// 게시물 조회
+postRouter.get("/get-posts", postService.getPosts());
+
+// 게시물 상세
+postRouter.get("/post-detail", postService.getPostById());
+
+export { postRouter };
