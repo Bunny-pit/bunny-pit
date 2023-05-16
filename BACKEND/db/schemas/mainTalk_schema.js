bc354ef2f433ab47a653c36b5a101636da8462ae
[SW4/1반] 이준미 <junmi950918@gmail.com>
2023-05-17 00:09:03 +0900

    feat : mainTalk schema 파일 추가

---
+++ BACKEND/db/schemas/mainTalk_schema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const postSchema = new Schema({
+  // 작성자
+  author: {
+    // 작성자 닉네임
+    userNickName: {
+      type: String,
+      required: true,
+    },
+    // 작성자 프로필 사진 url 경로
+    profilePicture: {
+      type: String,
+      required: true,
+    },
+  },
+  // 게시글 내용
+  content: {
+    type: String,
+    required: true,
+  },
+  // 생성 날짜
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
