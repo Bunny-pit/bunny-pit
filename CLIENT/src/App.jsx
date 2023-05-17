a2a5f6719d3818e6051726c38ea8d2af17b57147
H0onnn <dukei2017@naver.com>
2023-05-17 18:26:20 +0900

    git pull FE_dev

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,21 +1,20 @@
-
-
-import './App.css';
+import "./App.css";
 import { HomePage } from "./home_page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from './main/main_page';
+import Main from "./main/main_page";
 import "./App.css";
 
 function App() {
   return (
-    <Main />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-
 }
 
 export default App;
