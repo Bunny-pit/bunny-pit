b7d679a5191ed30f05a2243b15927ad32f304109
ryuiseo <rks85227@gmail.com>
2023-05-17 18:40:15 +0900

    feat : register page

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,21 +1,24 @@
-
-
-import './App.css';
+import "./App.css";
 import { HomePage } from "./home_page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from './main/main_page';
+import Main from "./main/main_page";
 import "./App.css";
+import LogIn from "./log_in/login_page";
+import Register from "./register/register";
 
 function App() {
   return (
-    <Main />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {/* <LogIn /> */}
+      <Register />
+      {/* <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter> */}
+    </>
   );
-
 }
 
 export default App;
