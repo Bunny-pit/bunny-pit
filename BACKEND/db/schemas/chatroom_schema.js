69ac108c68bc7d60a69fee05684af0fef91f1c4a
ChanghyeonO <ckdgus5189@gmail.com>
2023-05-19 17:50:10 +0900

    docs : chatRoom 최종 스키마 작성 완료

--- BACKEND/db/schemas/chatroom_schema.js
+++ BACKEND/db/schemas/chatroom_schema.js
@@ -1,25 +1,30 @@
 import { Schema } from "mongoose";
 
-const chatRoomSchema = new Schema({
-  user1: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  user2: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+const ChatRoomSchema = new Schema({
+  participants: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
   messages: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Chat",
-      default: [],
+      sender: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      message: {
+        type: String,
+        required: true,
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
     },
   ],
 });
 
-// 채팅방 스키마 정의
+// 채팅 메시지 스키마 정의
 
-export { chatRoomSchema };
+export { ChatRoomSchema };
