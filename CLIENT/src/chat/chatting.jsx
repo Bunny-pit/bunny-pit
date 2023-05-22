6c419bddaae52c21b1072589c4436da6bfb10294
Jonguk Lee <looke2930@gmail.com>
2023-05-22 14:57:34 +0900

    feat : 채팅 UI 진행중

--- CLIENT/src/chat/chatting.jsx
+++ CLIENT/src/chat/chatting.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 // import style from "./chatting.module.css";
 
@@ -8,6 +8,11 @@ export function Chatting(props) {
   console.log(params.userId);
   return (
     <>
+      <header>
+        <Link to="/chat">
+          <img src="assets/arrow-left.png" alt="뒤로가기 버튼" />
+        </Link>
+      </header>
       <h1>이 곳은 {params.userId}님과의 채팅방 입니다.</h1>
       <div>{props.date}</div>
     </>
