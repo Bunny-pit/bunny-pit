88ed683b9bf437c221322704172f9acf3ea143c9
ChanghyeonO <ckdgus5189@gmail.com>
2023-05-22 14:52:42 +0900

    feat : 웹소켓으로 실시간 데이터 받아오기 구현 중

--- BACKEND/db/services/chat_service.js
+++ BACKEND/db/services/chat_service.js
@@ -1,72 +1,66 @@
 import { ChatModel } from "../models/chat_model.js";
 
 class ChatService {
-  // 사용자 검색
   async searchUser(nickname) {
     try {
       const users = await ChatModel.findUser(nickname);
-      return users;
+      return { status: 200, data: users };
     } catch (error) {
       console.error("사용자 검색 중 오류 발생:", error);
-      throw error;
+      return { status: 500, error: "서버 오류" };
     }
   }
 
-  // 새로운 채팅방 개설
   async createChatRoom(loggedInUser, searchedUser) {
     try {
       const newChat = await ChatModel.createChatRoom(
         loggedInUser,
         searchedUser,
       );
-      return newChat;
+      return { status: 201, data: newChat };
     } catch (error) {
       console.error("채팅방 개설 중 오류 발생:", error);
-      throw error;
+      return { status: 500, error: "서버 오류" };
     }
   }
 
-  // 유저의 채팅방 전체 조회
   async getAllChatRoomsByUser(userId) {
     try {
       const chatRooms = await ChatModel.getAllChatRoomsByUser(userId);
-      return chatRooms;
+      return { status: 200, data: chatRooms };
     } catch (error) {
       console.error("채팅방 조회 중 오류 발생:", error);
-      throw error;
+      return { status: 500, error: "서버 오류" };
     }
   }
 
-  // 검색한 가입자와 채팅방에서 채팅 시작
   async startChat(chatId, sender, message) {
     try {
       const chatRoom = await ChatModel.startChat(chatId, sender, message);
-      return chatRoom;
+      return { status: 200, data: chatRoom };
     } catch (error) {
       console.error("채팅 시작 중 오류 발생:", error);
-      throw error;
+      return { status: 500, error: "서버 오류" };
     }
   }
 
-  // 채팅 삭제
   async deleteChat(chatId) {
     try {
       const result = await ChatModel.deleteChat(chatId);
-      return result;
+      return { status: 200, data: result };
     } catch (error) {
       console.error("채팅 삭제 중 오류 발생:", error);
-      throw error;
+      return { status: 500, error: "서버 오류" };
     }
   }
 
-  // 채팅방 삭제
   async deleteChatRoom(chatId) {
     try {
       const result = await ChatModel.deleteChatRoom(chatId);
-      return result;
+      return { status: 200, data: result };
     } catch (error) {
       console.error("채팅방 삭제 중 오류 발생:", error);
-      throw error;
+      return { status: 500, error: "서버 오류" };
     }
   }
 }
