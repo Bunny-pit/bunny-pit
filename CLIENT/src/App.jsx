43388a905a27094692c806d0792a514eac0a8c93
H0onnn <dukei2017@naver.com>
2023-05-25 21:05:06 +0900

    feat : main_page 컴포넌트 게시물 div 완료

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,19 +1,18 @@
 import "./App.css";
-import { HomePage } from "./home_page";
+import { HomePage } from "./home/home_page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./main/main_page";
 import PostDetail from "./post_detail/post_detail";
 function App() {
   return (
     <>
-      {/* <Main /> */}
-      <BrowserRouter>
+      <Main />
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/postDetail" element={<PostDetail />} />
         </Routes>
-      </BrowserRouter>
-
+      </BrowserRouter> */}
     </>
   );
 }
