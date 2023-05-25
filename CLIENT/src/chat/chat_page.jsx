9e3d037d8d06f3ff91315fd9e5b05197784a3d9d
Jonguk Lee <looke2930@gmail.com>
2023-05-25 22:33:06 +0900

    docs : chat_home.jsx 파일 수정

--- CLIENT/src/chat/chat_page.jsx
+++ CLIENT/src/chat/chat_page.jsx
@@ -1,7 +1,7 @@
-import React, { useNavigate } from "react";
+import React from "react";
 import chat_style from "./chat_style.module.css";
-import { Link } from "react-router-dom";
-// import Chatting from "./chatting";
+import Message from "./Message";
+// import { Chatting } from "./chatting";
 
 export function ChatPage() {
   /*
@@ -11,18 +11,23 @@ export function ChatPage() {
   시간,
   사진
   */
+
   return (
     <>
       <div className={chat_style.container}>
         <div className={chat_style.wrapper}>
-          <div className={chat_style.header}>
-            <button>
+          <header className={chat_style.header}>
+            <button
+              onClick={() => {
+                console.log("뒤로가기!!");
+              }}
+            >
               <img src="assets/arrow-left.png" alt="뒤로가기 버튼" />
             </button>
             <div className={chat_style.header_title}>
               <h2>채팅하기</h2>
             </div>
-          </div>
+          </header>
           <div className={chat_style.input_wrapper}>
             <input
               className={chat_style.input}
@@ -34,11 +39,6 @@ export function ChatPage() {
               }}
             />
           </div>
-          <Message
-            userId={"cute_hyeon"}
-            img={"assets/profile1.png"}
-            date={"1일"}
-          />
           <Message
             userId={"navi_rabbit12"}
             img={"assets/profile2.png"}
@@ -59,52 +59,8 @@ export function ChatPage() {
             img={"assets/profile5.png"}
             date={"1일"}
           />
-          <Message
-            userId={"cute_hansome_gang"}
-            img={"assets/profile5.png"}
-            date={"1일"}
-          />
-          <Message
-            userId={"cute_hansome_gang"}
-            img={"assets/profile5.png"}
-            date={"1일"}
-          />
-          <Message
-            userId={"cute_hansome_gang"}
-            img={"assets/profile5.png"}
-            date={"1일"}
-          />
-          <Message
-            userId={"cute_hansome_gang"}
-            img={"assets/profile5.png"}
-            date={"1일"}
-          />
         </div>
       </div>
     </>
-  );
-}
-
-function Message(props) {
-  // const navigate = useNavigate();
-  // const navigateToChat = () => {
-  //   navigate("/chat");
-  // };
-
-  return (
-    <Link to={`/chat/${props.userId}`} className={chat_style.message_container}>
-      <div
-        className={chat_style.message_wrapper}
-        onClick={() => {
-          console.log("test");
-        }}
-      >
-        <img src={props.img} alt="프로필 사진" />
-        <div className={chat_style.text}>
-          <div className={chat_style.message_id}>{props.userId}</div>
-          <div className={chat_style.message_date}>{props.date} 전에 보냄</div>
-        </div>
-      </div>
-    </Link>
   );
 }
